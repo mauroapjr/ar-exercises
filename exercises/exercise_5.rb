055717afd25bd50465601394c1c6ae0cdc3507a5
mauroapjr <mauroapjr@outlook.com>
2023-03-06 22:26:31 -0500

    done 5th task

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,20 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Total revenue from all stores are: $ #{Store.sum("annual_revenue").to_i}"
+puts "Average revenue from all stores are: $ #{Store.average("annual_revenue").to_i}"
+puts "Number of stores that are generating $1M or more: #{Store.where("annual_revenue > ?", 1000000).count.to_i}"
+
+
+
+
+
+
+
+
+
+
+
+
+
